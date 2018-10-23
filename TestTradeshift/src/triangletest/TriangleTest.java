3911f629fd7079a62862d9985e0831f458845788
Mary <Mary@DESKTOP-7KGNOOM.local>
2018-10-23 20:43:03 +0300

    change signature of checkTriangle

--- TestTradeshift/src/triangletest/TriangleTest.java
+++ TestTradeshift/src/triangletest/TriangleTest.java
@@ -5,7 +5,7 @@ public class TriangleTest {
 	public static void main(String[] args) {
 		
 		Triangle triangle = new Triangle (2.5,5.2,10.2);
-		TriangleUtils.checkTriangle(triangle);
+		TriangleUtils.checkTriangleType(triangle);
 		
 		
 		Triangle triangle1 = new Triangle (10.2, 15.2, 0.5);
