e7c8242a0ed21a87007f9595ddc04e4415ff875d
alex <bot.alexandru31@gmail.com>
2018-10-24 00:30:41 +0300

    stable version 1 - still needs some tests

--- TestTradeshift/src/triangletest/TriangleTest.java
+++ TestTradeshift/src/triangletest/TriangleTest.java
@@ -1,20 +1,50 @@
 package triangletest;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class TriangleTest {
-	
+
 	public static void main(String[] args) {
 		
-		Triangle triangle = new Triangle (2.5,5.2,10.2);
-		TriangleUtils.checkTriangleType(triangle);
-		
-		  
-		Triangle triangle1 = new Triangle (10.2, 15.2, 0.5);
-		
-		
-		
-		
-		
+		testTriangle(args);
 		
 	}
 
+	public static void testTriangle(String[] args) {
+		String repeatString = null;
+		Scanner userInput = null;
+		Triangle triangle = new Triangle();
+		String resultFromCheck = TriangleUtils.getUserInput(triangle, userInput);
+		if (resultFromCheck.equals("ok")) {
+			TriangleUtils.checkTriangleType(triangle);
+
+		} else {
+			try {
+				System.out.println("Check user input failed. Would you like to enter new values?(y/n)");
+				System.out.println("before scanner");
+				Scanner newScanner = new Scanner(System.in);
+				if (newScanner.hasNextLine()) {
+					repeatString = newScanner.nextLine();
+					if ("y".equals(repeatString)) {
+						main(args);
+					} else {
+						System.exit(0);
+					}
+				}
+			} catch (InputMismatchException inputMismatchEx) {
+				System.out.println("Please enter a number. [" + inputMismatchEx.getCause() + "]");
+			} catch (NoSuchElementException noElem) {
+				System.out.println(noElem.getMessage());
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			} finally {
+				if (userInput != null) {
+					System.out.println("closing repeatInput");
+					userInput.close();
+				}
+			}
+		}
+	}
 }
--- TestTradeshift/src/triangletest/TriangleUtils.java
+++ TestTradeshift/src/triangletest/TriangleUtils.java
@@ -1,17 +1,93 @@
 package triangletest;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class TriangleUtils {
 
-	
 	public static String checkTriangleType(Triangle triangle) {
-		System.out.println("<<entering method - checkTriangle - >>>");
 		String result = "";
-		System.out.println();
-		
-		  
-		
-		System.out.println("<<exiting method - checkTriangle - >>>");
+
+		Double cathetus1 = triangle.getCathetus1();
+		Double cathetus2 = triangle.getCathetus2();
+		Double hypotenuse = triangle.getHypotenuse();
+		System.out.println("The values are: "+triangle.toString());
+
+		if (cathetus1.equals(hypotenuse) || cathetus1.equals(cathetus2) || hypotenuse.equals(cathetus2)) {
+			result = TriangleType.ISOSCELES.toString();
+		}
+
+		if (!cathetus1.equals(cathetus2) && !cathetus2.equals(hypotenuse) && !hypotenuse.equals(cathetus1)) {
+			result = TriangleType.SCALENE.toString();
+		}
+
+		if (cathetus1.equals(cathetus2) && cathetus2.equals(hypotenuse)) {
+			result = TriangleType.EQUILATERAL.toString();
+		}
+		System.out.println("result: " + result);
+		System.out.println("");
 
 		return result;
 	}
+
+	public static String checkUserInput(Double cathetus1, Double cathetus2, Double hypotenuse) {
+		System.out.println("<<entering method - checkUserInput - >>>");
+		String message = null;
+
+		if (cathetus1 > 0 && cathetus2 > 0 && hypotenuse > 0) {
+			message = "ok";
+		} else {
+			message = "ko";
+			System.out.println("Check failed! Values should be > 0.");
+
+		}
+		System.out.println("<<exiting method - checkUserInput - >>>");
+
+		return message;
+
+	}
+
+	public static String getUserInput(Triangle triangle, Scanner userInput) {
+		System.out.println("...getting user input");
+		Double cathetus1 = 0D;
+		Double cathetus2 = 0D;
+		Double hypotenuse = 0D;
+		String result = null;
+
+		try {
+			userInput = new Scanner(System.in);
+			System.out.println(" Enter the value for the first cathetus:");
+			cathetus1 = userInput.nextDouble();
+			System.out.println(" Enter the value for the second cathetus: ");
+			cathetus2 = userInput.nextDouble();
+			System.out.println(" Enter the value for the hypotenuse: ");
+			hypotenuse = userInput.nextDouble();
+			String checkUserResult = checkUserInput(cathetus1, cathetus2, hypotenuse);
+			if ("ok".equals(checkUserResult)) {
+				System.out.println("setting values on triangle...");
+				triangle.setCathetus1(cathetus1);
+				triangle.setCathetus2(cathetus2);
+				triangle.setHypotenuse(hypotenuse);
+				result = "ok";
+			} else {
+				result = "ko";
+			}
+
+		} catch (InputMismatchException inputMismatchEx) {
+			System.out.println("Please enter a number. [" + inputMismatchEx.getCause() + "]");
+			if (userInput != null) {
+				System.out.println("closing userInput");
+				userInput.close();
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			if (userInput != null) {
+				System.out.println("closing userInput");
+				userInput.close();
+			}
+		}
+
+		return result;
+	}
+
 }
